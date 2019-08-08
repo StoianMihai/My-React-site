8d966931ae8525fa1a440fbe4949541cb1a766bb
Stoian Mihai <mihai_stoian21@yahoo.com>
2019-08-08 12:50:46 +0300

    added 404 page

--- src/pages/ArticlePage.js
+++ src/pages/ArticlePage.js
@@ -1,11 +1,16 @@
 import React from 'react';
+import ArticlesList from '../components/ArticlesList';
+import NotFoundPage from './NotFoundPage';
 import articleContent from './article-content';
 
 const ArticlePage = ({ match }) => {
+
     const name = match.params.name;
     const article = articleContent.find(article => article.name === name);
 
-    if (!article) return <h1>Article does not exist!</h1>
+    if (!article) return <NotFoundPage />
+
+    const otherArticles = articleContent.filter(article => article.name !== name);
 
     return (
     <>
@@ -13,10 +18,10 @@ const ArticlePage = ({ match }) => {
         {article.content.map((paragraf, key) => (
             <p key={key}>{paragraf}</p>
         ))}
+        <h3>Other Articles: </h3>
+        <ArticlesList articles={otherArticles} />
 
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed luctus ac elit semper pulvinar. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Quisque viverra convallis lacinia. Sed semper congue posuere. Proin gravida tempus eros sit amet scelerisque. Nulla faucibus neque vitae ex venenatis laoreet. Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-        <p>Phasellus porta convallis sapien ut sagittis. Duis tristique nibh felis, ac pharetra ipsum varius ut. Curabitur aliquam sapien non venenatis ultrices. Vestibulum consequat sapien a eros sodales, at bibendum nibh vehicula. Sed vestibulum bibendum auctor. Sed pellentesque nibh orci, accumsan lobortis tellus eleifend eget. Nam pulvinar quam eu suscipit rhoncus.</p>
-
+       
     </>
        
 
